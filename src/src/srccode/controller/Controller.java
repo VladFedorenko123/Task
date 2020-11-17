480e3b6eb82e6e3bc83f5c52b674e97a223c99c1
VladFedorenko123 <vladfeddorenko@gamil.com>
2020-11-17 13:12:12 +0200

    Task
    Add you own HttpServlet handlers that will process GET, POST, PUT and DELETE requests

---
+++ src/src/srccode/controller/Controller.java
@@ -0,0 +1,61 @@
+package src.srccode.controller;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Controller
+ */
+@WebServlet("/Controller")
+public class Controller extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+    
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String a = "doGet";
+		HelperMethod.sendResponse(a, request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String a = "doPost";
+		HelperMethod.sendResponse(a, request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String a = "doPut";
+		HelperMethod.sendResponse(a, request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
+	 */
+	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String a = "doDelete";
+		HelperMethod.sendResponse(a, request, response);
+	}
+
+}
+class HelperMethod{
+	public static String sendResponse(String a,HttpServletRequest request, HttpServletResponse response) throws IOException {
+		response.setContentType("text/html");
+		PrintWriter writer = response.getWriter();
+		
+		writer.println(a);
+		return a;
+	}
+}
