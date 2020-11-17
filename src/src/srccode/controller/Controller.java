8777d082167f503aae822aeeacfa55b238ade58d
VladFedorenko123 <vladfeddorenko@gamil.com>
2020-11-17 15:07:13 +0200

    Task
    Add you own HttpServlet handlers that will process GET, POST, PUT and DELETE requests

--- src/src/srccode/controller/Controller.java
+++ src/src/srccode/controller/Controller.java
@@ -4,58 +4,44 @@ import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class Controller
- */
+
 @WebServlet("/Controller")
 public class Controller extends HttpServlet {
 	private static final long serialVersionUID = 1L;
     
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String a = "doGet";
-		HelperMethod.sendResponse(a, request, response);
+		sendResponse("doGet", request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
+	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String a = "doPost";
-		HelperMethod.sendResponse(a, request, response);
+		sendResponse("doPost", request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doPut(HttpServletRequest, HttpServletResponse)
-	 */
+	
 	protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String a = "doPut";
-		HelperMethod.sendResponse(a, request, response);
+		sendResponse("doPut", request, response);
 	}
 
-	/**
-	 * @see HttpServlet#doDelete(HttpServletRequest, HttpServletResponse)
-	 */
+	
 	protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String a = "doDelete";
-		HelperMethod.sendResponse(a, request, response);
+		sendResponse("doDelete", request, response);
 	}
-
-}
-class HelperMethod{
-	public static String sendResponse(String a,HttpServletRequest request, HttpServletResponse response) throws IOException {
+	
+	public void sendResponse(String string, HttpServletRequest request, HttpServletResponse response) throws IOException {	
 		response.setContentType("text/html");
-		PrintWriter writer = response.getWriter();
 		
-		writer.println(a);
-		return a;
+		try(PrintWriter writer = response.getWriter()) {
+			writer.println(string);
+		}
 	}
 }
+
